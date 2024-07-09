ff5cc9ee7b84ae322f341b93c45ed8afcc3886db
vikaschahar80 <147334626+vikaschahar80@users.noreply.github.com>
2024-07-09 17:29:58 +0530

    pascal's triangle

---
+++ 17.cpp
@@ -0,0 +1,26 @@
+vector<int> generateRow(int row)
+{
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for(int i=1;i<row;i++)
+    {
+        ans = ans * (row - i);
+        ans = ans / i;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v;
+        for(int i=1;i<=numRows;i++)
+        {
+            vector<int> temp = generateRow(i);
+            v.push_back(temp);
+        }
+        return v;
+    }
+};
