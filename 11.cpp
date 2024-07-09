e22319da3363a6c8f831d7e008013233996a09cf
vikaschahar80 <147334626+vikaschahar80@users.noreply.github.com>
2024-07-09 14:50:08 +0530

    climbing_stairs

---
+++ 11.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a[46];
+        a[1]=1;
+        a[2]=2;
+        for(int i =3;i<=n;i++){
+            a[i] = a[i-1]+a[i-2];
+        }
+        return a[n];
+
+    }
+};
